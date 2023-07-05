8687f6a4b2dd8feb421566a6b85670fe0320407d
mieng18 <mieng18@hotmail.com>
2023-07-05 15:42:35 -0700

    Initial commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by mai nguyen on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
